da60276811f6b2d2ed5ef19af282a238dfc0938f
Xunto <hunterdelyx1@gmail.com>
2016-12-28 01:19:32 +0300

    use dict_size

--- lz78.py
+++ lz78.py
@@ -1,44 +1,79 @@
-def find_in_dict(string, dictionary):
+from typing import List, AnyStr
+
+
+class DictEntry:
+    def __init__(self, substring=""):
+        self.substring = substring
+        self.uses = 0
+
+    def increment_uses(self):
+        self.uses += 1
+
+    def new_with(self, character: AnyStr):
+        return DictEntry(self.substring + character)
+
+    def __repr__(self):
+        return "\"" + self.substring + "\""
+
+
+def find_in_dict(string: AnyStr, dictionary: List[DictEntry]):
     number = 0
+    tmp_size = 0
     for i in range(number, len(dictionary)):
         from_dict = dictionary[i]
-        size = len(from_dict)
+        size = len(from_dict.substring)
 
-        if from_dict == string[:size]:
+        if from_dict.substring == string[:size] and size > tmp_size:
+            tmp_size = size
             number = i
 
     return number
 
 
 def compress(message, dictionary_size=12):
-    dictionary = [""]
+    if dictionary_size <= 1:
+        raise ValueError("dictionary size should be 2 or greater")
 
-    test = ""
+    dictionary = [DictEntry()]
+
     output = []
     while len(message) > 0:
         number = find_in_dict(message, dictionary)
-        size = len(dictionary[number])
+        matching_entry = dictionary[number]
+        matching_entry.increment_uses()
 
-        message = message[size:]
+        message = message[len(matching_entry.substring):]
 
         character = message[:1]
         message = message[1:]
 
-        dictionary.append(dictionary[number] + character)
-        test += dictionary[number] + character
+        dictionary.append(matching_entry.new_with(character))
+
         output.append((number, character))
+        if len(dictionary) >= dictionary_size:
+            remove_entry = sorted(dictionary, key=lambda entry: (entry.uses, len(entry.substring)))[0]
+            dictionary.remove(remove_entry)
 
     return output
 
 
-def decompress(compressed_message):
-    message = ""
+def decompress(compressed_message, dictionary_size=12):
+    if dictionary_size <= 1:
+        raise ValueError("dictionary size should be 2 or greater")
 
-    dictionary = [""]
+    message = ""
+    dictionary = [DictEntry()]
     for part in compressed_message:
         number, character = part
+        current_entry = dictionary[number]
+        current_entry.increment_uses()
 
-        substring = dictionary[number] + character
-        dictionary.append(substring)
-        message += substring
+        new_entry = current_entry.new_with(character)
+        dictionary.append(new_entry)
+        message += new_entry.substring
+
+        if len(dictionary) >= dictionary_size:
+            remove_entry = sorted(dictionary, key=lambda entry: (entry.uses, len(entry.substring)))[0]
+            dictionary.remove(remove_entry)
+
     return message
--- tests.py
+++ tests.py
@@ -19,27 +19,34 @@ tests = [
     }
 ]
 
+# Ordinary
 for test in tests:
     result = lz78.compress(test['message'], test['dict_size'])
     print(result)
     assert result == test['result']
 
-    message = lz78.decompress(test['result'])
+    message = lz78.decompress(test['result'], test['dict_size'])
     print(message)
     assert message == test['message']
     print('===========================')
 
 
+# Test with random
 for i in range(0, 1000):
+    dict_size = random.randint(2, 20)
     case = "".join(
         random.choice(
             string.ascii_uppercase + string.ascii_lowercase + string.digits
         ) for x in range(random.randint(1, 1000))
     )
+    print(case)
+    print(dict_size)
 
-    result = lz78.compress(case, 16)
+    result = lz78.compress(case, dict_size)
     print(result)
-    message = lz78.decompress(result)
+
+    message = lz78.decompress(result, dict_size)
     print(message)
+
     assert message == case
     print('===========================')
